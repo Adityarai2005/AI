0a77774f5bc30993078a5bc496c59ac802ce6d5e
Adityarai2005 <151074183+Adityarai2005@users.noreply.github.com>
2025-06-26 19:56:39 +0530

    Update files.py

--- Python/files.py
+++ Python/files.py
@@ -7,6 +7,6 @@ def write_txt(file_path,text):
 result=""
 print(result)
 write_txt(r"C:\Users\ar252\.vscode\Python\sample.txt","I am Hardworking person who only do hardwork when it is too late")
-result+=read_text(r"C:\Users\ar252\.vscode\Python\sample.txt")
+result+=read_text(r"file path hidden for security purposes")
 print(result)
 # similary specific modules are made to read pdf ,doc files
